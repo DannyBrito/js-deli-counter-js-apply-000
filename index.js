88e44b66376bca31f89c35e3be40daa7cffc6a12
Danny Brito <britodanny1997@gmail.com>
2019-09-16 18:03:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function takeANumber(line, name){
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`
+}
+function nowServing(katzDeliLine){
+  if(katzDeliLine[0] === undefined) return 'There is nobody waiting to be served!';
+  return `Currently serving ${katzDeliLine.shift()}.`;
+}
+function currentLine(line){
+  if(line[0] === undefined) return 'The line is currently empty.';
+  var result ='The line is currently: ';
+  for(let i = 0; i < line.length; ++i){
+  result +=i+1;
+  result +='. ' +line[i];
+  if(i === line.length-1);
+  else result+= ', '
+  }
+  return result;
+}
